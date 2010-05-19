c1124a96a45057586be9bef5121e3f3a46dffa6a
djx.zhenghua <djx.zhenghua@13af8306-5bc8-11de-8a4b-256bef446c6c>
2010-05-19 14:27:01 +0000

    add winthread

--- trunk/include/threadWrapper.h
+++ trunk/include/threadWrapper.h
@@ -23,6 +23,16 @@
 #define THREAD_NUM 2
 #endif
 
+
+#if (!defined(_PTHREAD)) && (!defined(_WINTHREAD))
+	#if defined(__GNUC__)
+		#define _PTHREAD
+	#elif defined(_MSC_VER)
+		#define _WINTHREAD 
+	#endif
+#endif
+
+#ifdef  _PTHREAD
 #include <pthread.h>
 
 #define defaultMutex(x)\
@@ -31,22 +41,26 @@
 #define defaultCond(x)\
 	pthread_cond_t x= PTHREAD_COND_INITIALIZER;
 
+#define defaultBarrier(x)\
+	pthread_barrier_t x;
+
 #define THREAD_VAR \
-pthread_t tTable[THREAD_NUM]; \
-pthread_barrier_t tBarrier;\
-pthread_mutex_t tMutex=PTHREAD_MUTEX_INITIALIZER;\
+pthread_t tTable[THREAD_NUM*2]; \
+defaultBarrier(tBarrier);\
+defaultMutex(tMutex);\
 defaultCond(tCond);\
 static int tNum=0;
 
 #define create_threads(pfunc, arg) {\
 	pthread_barrier_init(&tBarrier, NULL, THREAD_NUM);\
 	for(;tNum<THREAD_NUM;tNum++){\
+		unsigned int tid = tNum;\
 		int ret =pthread_create(&tTable[tNum], NULL, (void *(*)(void *))pfunc, arg);\
 	}\
 }
 
 #define create_thread(pfunc, arg) \
-{int ret = pthread_create(&tTable[tNum++], NULL, (void *(*)(void *))pfunc, arg);}
+{unsigned int tid = tNum; int ret = pthread_create(&tTable[tNum++], NULL, (void *(*)(void *))pfunc, arg);}
 
 #define invoke(pfunc) \
 {int ret = pthread_create(&tTable[tNum++], NULL, (void *(*)(void *))pfunc, NULL);}
@@ -65,4 +79,73 @@ static int tNum=0;
 #define condP(cond_s, cond_mutex) pthread_cond_wait(&cond_s, &cond_mutex);
 #define condV(cond_s) pthread_cond_signal(&cond_s);
 #define Barrier() pthread_barrier_wait(&tBarrier);
+
+#define tfunc_ret void* 
+/****************************************************************************************/
+/**************************pthread*******************************************************/
+/****************************************************************************************/
+#else 
+/****************************************************************************************/
+/**************************win thread****************************************************/
+/****************************************************************************************/
+#include <windows.h>
+#include <process.h>
+
+#define defaultMutex(x)\
+	HANDLE x=0;
+
+#define defaultCond(x)\
+	HANDLE x =0;
+
+#define defaultBarrier(x)\
+	HANDLE x=0;
+
+#define THREAD_VAR \
+HANDLE tTable[THREAD_NUM*2]; \
+HANDLE tBarrier;\
+HANDLE tMutex;\
+defaultCond(tCond);\
+static int tNum=0;
+
+#define create_threads(pfunc, arg) {\
+	tMutex= CreateMutex(NULL,FALSE , 0);\
+	tBarrier = CreateEvent(NULL, TRUE, FALSE, NULL);\
+	for(;tNum<THREAD_NUM;tNum++){\
+		unsigned int threadid;\
+		unsigned int tid = tNum;\
+        tTable[tNum] = (HANDLE)_beginthreadex(NULL, 0, pfunc, arg, 0, &threadid );\
+	}\
+}
+
+#define create_thread(pfunc, arg) \
+        {unsigned int tid = tNum; tTable[tNum++] = (HANDLE)_beginthreadex(NULL, 0, pfunc, arg, 0, NULL);}
+
+#define invoke(pfunc) \
+        {tTable[tNum++] = (HANDLE)_beginthreadex(NULL, 0, pfunc, NULL, 0, NULL);}
+
+#define waitall_threads() {\
+	int i=0;\
+	WaitForMultipleObjects(tNum, tTable, TRUE, INFINITE);\
+	for(i=0;i<tNum;i++){\
+	CloseHandle(tTable[i]);\
+	}\
+	if(tMutex) CloseHandle(tMutex);\
+	if(tBarrier) CloseHandle(tBarrier);\
+	if(tCond) CloseHandle(tCond);\
+}
+
+
+
+#define P(s) WaitForSingleObject(s, INFINITE); 
+#define V(s) ReleaseSemaphore(s, 1 ,0)
+#define tP(s) pthread_mutex_trylock(&s); 
+#define p() WaitForSingleObject(tMutex,INFINITE); 
+#define v() ReleaseSemaphore(&tMutex, 1, 0)
+#define condP(cond_s, cond_mutex) pthread_cond_wait(&cond_s, &cond_mutex);
+#define condV(cond_s) pthread_cond_signal(&cond_s);
+#define Barrier()  //WaitForSingleObject(tBarrier, INFINITE);
+
+#define tfunc_ret unsigned WINAPI
+#endif   /* ----- #ifndef _PTHREAD  ----- */
+
 #endif   /* ----- #ifndef THREADWRAPPER_HEADER__INC  ----- */
