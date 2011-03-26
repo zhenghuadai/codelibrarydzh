02003b2853c0e3c2e54a6f601c0fbc4c276bba08
djx.zhenghua <djx.zhenghua@13af8306-5bc8-11de-8a4b-256bef446c6c>
2011-03-26 01:37:12 +0000

    add install

---
+++ trunk/example/dcsh.example.foo.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testfoo.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2011 02:28:57 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  DAI ZHENGHUA (), djx.zhenghua@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+void foo()
+{
+    printf("fooc\n");
+}
---
+++ trunk/example/dcsh.example.main.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  03/23/2011 02:28:10 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  DAI ZHENGHUA (), djx.zhenghua@gmail.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+//!require "testfoo.c"
+extern void foo();
+int main()
+{
+    foo();
+}
